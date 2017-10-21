3a8c682bb6b3d79bfe3f5f8c16428f7eff934336
ibon <ibondiezlukas@gmail.com>
2017-10-21 06:19:20 +0200

    arduino programa

--- Arduino/programa/after_adding_posicion_cleanig_funciones/after_adding_posicion_cleanig_funciones.ino
+++ Arduino/programa/after_adding_posicion_cleanig_funciones/after_adding_posicion_cleanig_funciones.ino
@@ -4,12 +4,19 @@
    Dev: Michalis Vasilakis // Date: 1/7/2015 // www.ardumotive.com */
 
 //Libraries
-#include <DHT.h>;
+#include <DHT.h>
 
 //Constants
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+String typoPapelera = "paper";
+String posit = "40.446;79.982";
+String id = "01";
+
+
 const unsigned int TRIG_PIN=3;
 const unsigned int ECHO_PIN=4;
 const unsigned int BAUD_RATE=9600;
@@ -19,37 +26,99 @@ int duration=0;
 int chk;
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
+const int buttonPin = 5;     // the number of the pushbutton pin
+const int firePin = 6;     
+long buttonTimer = 0;
+long longPressTime = 500;
 
+boolean buttonActive = false;
+boolean longPressActive = false;
+boolean fire = false;
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+
+String inputString = "";         // a String to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 void setup()
 {
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
     Serial.begin(BAUD_RATE);
     dht.begin();
+    // initialize the pushbutton pin as an input:
+    pinMode(buttonPin, INPUT);
+    pinMode(firePin, INPUT);
+      // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
 
 }
 
 void loop()
 {
+  bool lleno;
+if (digitalRead(buttonPin) == LOW) {
+
+    if (buttonActive == false) {
+
+      buttonActive = true;
+      buttonTimer = millis();
+
+    }
+
+    if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
+
+      longPressActive = true;
+
+      print_id();
+      print_pos_type();
+
+      
+    }
+
+  } else {
+
+    if (buttonActive == true) {
+
+      if (longPressActive == true) {
+
+        longPressActive = false;
+
+      } else {
+          //Read humidity and store it to variables hum 
+          hum = dht.readHumidity();
+          
+          //Read temperature and store it to variables temp
+          temp= dht.readTemperature();
+          
+          //Calculate duration
+          distance = calculate_duration();
+          if(distance < 10) lleno = HIGH;
+          else lleno = LOW;
+
+          fire = digitalRead(firePin);
+
+          print_id();
+        //print humidity and temperature
+          print_hum_and_temp(hum,temp);
+          
+          //print distance 
+          print_distance (lleno);
+
+          print_fire(fire);
+}
+
+      buttonActive = false;
+
+    }
+
+  }
+  if (stringComplete) {
     
-    //Read humidity and store it to variables hum 
-    hum = dht.readHumidity();
-    
-    //Read temperature and store it to variables temp
-    temp= dht.readTemperature();
-    
-    //print humidity and temperature
-    print_hum_and_temp(hum,temp);
-    
-    //Calculate duration
-    duration = calculate_duration();
-    
-    //Calculate distance
-    distance= duration/29/2;
-    
-    //print distance 
-    print_distance (distance, duration);
-     
+  }
 }
 
 
@@ -58,30 +127,25 @@ void loop()
 void print_hum_and_temp(int hum0,int temp0)
   {
       //Print temp and humidity values to serial monitor
-      Serial.print("Humidity: ");
+      Serial.print("$hum:");
       Serial.print(hum0);
-      Serial.print(" %, Temp: ");
+      Serial.print("$temp:");
       Serial.print(temp0);
-      Serial.println(" Celsius");
-      delay(2000); //Delay 2 sec.
+
   }
 
 
 
-void print_distance(int duration0, int distance0)
+void print_distance(int container)
 {
-     if(duration0==0){
-        Serial.println("Warning: no pulse from sensor");
-     } 
-     else{
-        Serial.print("distance to nearest object:");
-        Serial.print(distance0);
-        Serial.println(" cm");
-    }
-    delay(100);
+        Serial.print("$lleno:");
+        Serial.print(container);
 }  
-
-
+void print_fire(bool fire)
+{
+        Serial.print("$fire:");
+        Serial.print(fire);
+} 
 int calculate_duration(){
   // sensor de posicion 
     
@@ -92,9 +156,19 @@ int calculate_duration(){
     digitalWrite(TRIG_PIN, LOW);  
 
     const unsigned long duration0= pulseIn(ECHO_PIN, HIGH);
-    //int distance= duration0/29/2;
+    int distance= duration0/29/2;
 
-    return duration0;
+    return distance;
     }
+void print_pos_type(){
+        Serial.print("$type:");
+        Serial.print(typoPapelera);
+        Serial.print("$pos:");
+        Serial.print(posit);
+ }
+ void print_id(){
+        Serial.print("$id:");
+        Serial.print(id);
+ }
 
- 
+
