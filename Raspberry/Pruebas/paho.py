c0cfe43e483e434c675e36fead944523d5a9cbc4
Carlos <cquerol@morava-labs.com>
2017-10-21 15:26:40 +0200

    Añadido main.py con envio de datos a IoT hub funcional

--- Raspberry/Pruebas/paho.py
+++ Raspberry/Pruebas/paho.py
@@ -1,254 +1,56 @@
-#!/usr/bin/env python
+from paho.mqtt import client as mqtt                                # be sure to install package paho-mqtt
+import ssl
+########## You need only modify this section start ##########
+# You can generate the SAS with Device Explorer
+# Example SharedAccessSignature = "SharedAccessSignature sr=kevinsay.azure-devices.net%2fdevices%2fmygreatdevice&sig=ON2EYJR1hUC84rJSMe%2fCrBdb8lcIr7A%3d&se=15055292"
+# SharedAccessSignature = "SharedAccessSignature sr=kevinsayIoT.azure-devices.net%2fdevices%2fpython&sig=ON2EYJR7R7ILlA1ChUC84rJSMWEBe%2fCJrBdb8lcIr7A%3d&se=1505515292"
+ConnectionString = "HostName=ValenHub.azure-devices.net;DeviceId=Raspberry01;SharedAccessKey=dop/dAKF//LMLfpIwjLxG1mTnHfofwtVwN2O4nWmfBw="
 
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for
-# full license information.
-
-import random
-import time
-import sys
-from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
-import config as config
-from BME280SensorSimulator import BME280SensorSimulator
-import RPi.GPIO as GPIO
-from Adafruit_BME280 import *
-import re
-from telemetry import Telemetry
-
-# HTTP options
-# Because it can poll "after 9 seconds" polls will happen effectively
-# at ~10 seconds.
-# Note that for scalabilty, the default value of minimumPollingTime
-# is 25 minutes. For more information, see:
-# https://azure.microsoft.com/documentation/articles/iot-hub-devguide/#messaging
-TIMEOUT = 241000
-MINIMUM_POLLING_TIME = 9
-
-# messageTimeout - the maximum time in milliseconds until a message times out.
-# The timeout period starts at IoTHubClient.send_event_async.
-# By default, messages do not expire.
-MESSAGE_TIMEOUT = 10000
-
-RECEIVE_CONTEXT = 0
-MESSAGE_COUNT = 0
-MESSAGE_SWITCH = True
-TWIN_CONTEXT = 0
-SEND_REPORTED_STATE_CONTEXT = 0
-METHOD_CONTEXT = 0
-TEMPERATURE_ALERT = 30.0
-
-# global counters
-RECEIVE_CALLBACKS = 0
-SEND_CALLBACKS = 0
-BLOB_CALLBACKS = 0
-TWIN_CALLBACKS = 0
-SEND_REPORTED_STATE_CALLBACKS = 0
-METHOD_CALLBACKS = 0
-EVENT_SUCCESS = "success"
-EVENT_FAILED = "failed"
-
-# chose HTTP, AMQP or MQTT as transport protocol
-PROTOCOL = IoTHubTransportProvider.MQTT
-
-# String containing Hostname, Device Id & Device Key in the format:
-# "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-telemetry = Telemetry()
-
-if len(sys.argv) < 2:
-    print ( "You need to provide the device connection string as command line arguments." )
-    telemetry.send_telemetry_data(None, EVENT_FAILED, "Device connection string is not provided")
-    sys.exit(0)
-
-def is_correct_connection_string():
-    m = re.search("HostName=.*;DeviceId=.*;", CONNECTION_STRING)
-    if m:
-        return True
-    else:
-        return False
-
-CONNECTION_STRING = sys.argv[1]
-
-if not is_correct_connection_string():
-    print ( "Device connection string is not correct." )
-    telemetry.send_telemetry_data(None, EVENT_FAILED, "Device connection string is not correct.")
-    sys.exit(0)
-
-MSG_TXT = "{\"deviceId\": \"Raspberry Pi - Python\",\"temperature\": %f,\"humidity\": %f}"
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(config.GPIO_PIN_ADDRESS, GPIO.OUT)
-
-def receive_message_callback(message, counter):
-    global RECEIVE_CALLBACKS
-    message_buffer = message.get_bytearray()
-    size = len(message_buffer)
-    print ( "Received Message [%d]:" % counter )
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode("utf-8"), size) )
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    counter += 1
-    RECEIVE_CALLBACKS += 1
-    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
-    return IoTHubMessageDispositionResult.ACCEPTED
+# download the cer from https://ssl-tools.net/certificates/d4de20d05e66fc53fe1a50882c78db2852cae474.pem
+# BaltimoreCyberTrustRootCER = "C:\\Users\\kevinsay\\desktop\\d4de20d05e66fc53fe1a50882c78db2852cae474.cer"
 
 
-def send_confirmation_callback(message, result, user_context):
-    global SEND_CALLBACKS
-    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
-    map_properties = message.properties()
-    print ( "    message_id: %s" % message.message_id )
-    print ( "    correlation_id: %s" % message.correlation_id )
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    SEND_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
-    led_blink()
+########### You need only modify this section end ###########
 
 
-def device_twin_callback(update_state, payload, user_context):
-    global TWIN_CALLBACKS
-    print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
-    TWIN_CALLBACKS += 1
-    print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
+# HubName = SharedAccessSignature.split("%2f")[0].split("=")[1]       # extracting the Hub name from the SAS
+# devicename = SharedAccessSignature.split("%2f")[2].split("&")[0]    # extracting the device name from the SAS
+
+HubName = ConnectionString.split(';')[0].split('=')[1]
+devicename = ConnectionString.split(';')[1].split('=')[1]
+signature = ConnectionString.split(';')[2].split('=')[1]
+
+print HubName
+print devicename
+print signature
+
+def on_connect(client, userdata, flags, rc):
+    print ("Connected with result code: " + str(rc))
+    client.subscribe("devices/" + devicename + "/messages/devicebound/#")
 
 
-def send_reported_state_callback(status_code, user_context):
-    global SEND_REPORTED_STATE_CALLBACKS
-    print ( "Confirmation for reported state received with:\nstatus_code = [%d]\ncontext = %s" % (status_code, user_context) )
-    SEND_REPORTED_STATE_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_REPORTED_STATE_CALLBACKS )
+def on_disconnect(client, userdata, rc):
+    print ("Disconnected with result code: " + str(rc))
 
 
-def device_method_callback(method_name, payload, user_context):
-    global METHOD_CALLBACKS,MESSAGE_SWITCH
-    print ( "\nMethod callback called with:\nmethodName = %s\npayload = %s\ncontext = %s" % (method_name, payload, user_context) )
-    METHOD_CALLBACKS += 1
-    print ( "Total calls confirmed: %d\n" % METHOD_CALLBACKS )
-    device_method_return_value = DeviceMethodReturnValue()
-    device_method_return_value.response = "{ \"Response\": \"This is the response from the device\" }"
-    device_method_return_value.status = 200
-    if method_name == "start":
-        MESSAGE_SWITCH = True
-        print ( "Start sending message\n" )
-        device_method_return_value.response = "{ \"Response\": \"Successfully started\" }"
-        return device_method_return_value
-    if method_name == "stop":
-        MESSAGE_SWITCH = False
-        print ( "Stop sending message\n" )
-        device_method_return_value.response = "{ \"Response\": \"Successfully stopped\" }"
-        return device_method_return_value
-    return device_method_return_value
+def on_message(client, userdata, msg):
+    print (msg.topic+" "+str(msg.payload))
+    # Do this only if you want to send a reply message every time you receive one
+    client.publish("devices/" + devicename + "/messages/events/", "{id=1}", qos=1)
 
 
-def blob_upload_conf_callback(result, user_context):
-    global BLOB_CALLBACKS
-    print ( "Blob upload confirmation[%d] received for message with result = %s" % (user_context, result) )
-    BLOB_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % BLOB_CALLBACKS )
+def on_publish(client, userdata, mid):
+    print ("Sent message")
 
-
-def iothub_client_init():
-    # prepare iothub client
-    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
-    if client.protocol == IoTHubTransportProvider.HTTP:
-        client.set_option("timeout", TIMEOUT)
-        client.set_option("MinimumPollingTime", MINIMUM_POLLING_TIME)
-    # set the time until a message times out
-    client.set_option("messageTimeout", MESSAGE_TIMEOUT)
-    # to enable MQTT logging set to 1
-    if client.protocol == IoTHubTransportProvider.MQTT:
-        client.set_option("logtrace", 0)
-    client.set_message_callback(
-        receive_message_callback, RECEIVE_CONTEXT)
-    if client.protocol == IoTHubTransportProvider.MQTT or client.protocol == IoTHubTransportProvider.MQTT_WS:
-        client.set_device_twin_callback(
-            device_twin_callback, TWIN_CONTEXT)
-        client.set_device_method_callback(
-            device_method_callback, METHOD_CONTEXT)
-    return client
-
-
-def print_last_message_time(client):
-    try:
-        last_message = client.get_last_message_receive_time()
-        print ( "Last Message: %s" % time.asctime(time.localtime(last_message)) )
-        print ( "Actual time : %s" % time.asctime() )
-    except IoTHubClientError as iothub_client_error:
-        if iothub_client_error.args[0].result == IoTHubClientResult.INDEFINITE_TIME:
-            print ( "No message received" )
-        else:
-            print ( iothub_client_error )
-
-
-def iothub_client_sample_run():
-    try:
-        client = iothub_client_init()
-
-        if client.protocol == IoTHubTransportProvider.MQTT:
-            print ( "IoTHubClient is reporting state" )
-            reported_state = "{\"newState\":\"standBy\"}"
-            client.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, SEND_REPORTED_STATE_CONTEXT)
-
-        if not config.SIMULATED_DATA:
-            sensor = BME280(address = config.I2C_ADDRESS)
-        else:
-            sensor = BME280SensorSimulator()
-
-        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_SUCCESS, "IoT hub connection is established")
-        while True:
-            global MESSAGE_COUNT,MESSAGE_SWITCH
-            if MESSAGE_SWITCH:
-                # send a few messages every minute
-                print ( "IoTHubClient sending %d messages" % MESSAGE_COUNT )
-                temperature = sensor.read_temperature()
-                humidity = sensor.read_humidity()
-                msg_txt_formatted = MSG_TXT % (
-                    temperature,
-                    humidity)
-                print (msg_txt_formatted)
-                message = IoTHubMessage(msg_txt_formatted)
-                # optional: assign ids
-                message.message_id = "message_%d" % MESSAGE_COUNT
-                message.correlation_id = "correlation_%d" % MESSAGE_COUNT
-                # optional: assign properties
-                prop_map = message.properties()
-                prop_map.add("temperatureAlert", "true" if temperature > TEMPERATURE_ALERT else "false")
-
-                client.send_event_async(message, send_confirmation_callback, MESSAGE_COUNT)
-                print ( "IoTHubClient.send_event_async accepted message [%d] for transmission to IoT Hub." % MESSAGE_COUNT )
-
-                status = client.get_send_status()
-                print ( "Send status: %s" % status )
-                MESSAGE_COUNT += 1
-            time.sleep(config.MESSAGE_TIMESPAN / 1000.0)
-
-    except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
-        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_FAILED, "Unexpected error %s from IoTHub" % iothub_error)
-        return
-    except KeyboardInterrupt:
-        print ( "IoTHubClient sample stopped" )
-
-    print_last_message_time(client)
-
-def led_blink():
-    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.HIGH)
-    time.sleep(config.BLINK_TIMESPAN / 1000.0)
-    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.LOW)
-
-def usage():
-    print ( "Usage: iothub_client_sample.py -p <protocol> -c <connectionstring>" )
-    print ( "    protocol        : <amqp, amqp_ws, http, mqtt, mqtt_ws>" )
-    print ( "    connectionstring: <HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>>" )
-
-def parse_iot_hub_name():
-    m = re.search("HostName=(.*?)\.", CONNECTION_STRING)
-    return m.group(1)
-
-if __name__ == "__main__":
-    print ( "\nPython %s" % sys.version )
-    print ( "IoT Hub Client for Python" )
-
-    iothub_client_sample_run()
+client = mqtt.Client(client_id=devicename, protocol=mqtt.MQTTv311)
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.on_publish = on_publish
+#client.username_pw_set(username=HubName + "/" + devicename, password=signature)
+client.username_pw_set(username=HubName , password=signature)
+#client.username_pw_set(username=HubName + "/" + devicename, password=SharedAccessSignature)
+#client.tls_insecure_set(False)
+#client.tls_set(ca_certs=BaltimoreCyberTrustRootCER, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+client.connect(HubName, port=8883 )
+client.loop_forever()
--- Raspberry/main.py
+++ Raspberry/main.py
@@ -1,5 +1,114 @@
 #!/usr/bin/python
 
+import time
+import ssl
+import serial
+
+from paho.mqtt import client as mqtt
+#from paho.mqtt import client as mqtt
+
+
 #
-#   Main entry point for the collection application
-#
+
+########## You need only modify this section start ##########
+# You can generate the SAS with Device Explorer Example SharedAccessSignature = "SharedAccessSignature
+#sr=kevinsay.azure-devices.net%2fdevices%2fmygreatdevice&sig=ON2EYJR1hUC84rJSMe%2fCrBdb8lcIr7A%3d&se=15055292" SharedAccessSignature = "SharedAccessSignature
+#sr=ValenHub.azure-devices.net&sig=B7Hs6nZos4veopgl8OdP%2FHZdUK35GUEht3DD6V8c%2F%2F8%3D&se=1540115522&skn=iothubowner"
+
+SharedAccessSignature = "SharedAccessSignature sr=ValenHub.azure-devices.net%2Fdevices%2FRaspberry01&sig=ueFtRoaew0HPO6lJgowKe00eL%2FGDlwfBEHnEUK9o6q4%3D&se=1540119980"
+
+# download the cer from https://ssl-tools.net/certificates/d4de20d05e66fc53fe1a50882c78db2852cae474.pem
+BaltimoreCyberTrustRootCER = "iot.cer"
+########### You need only modify this section end ###########
+hubName = SharedAccessSignature.split("%2F")[0].split("=")[1]
+deviceName = SharedAccessSignature.split("%2F")[2].split("&")[0]
+#deviceName = "raspberry01"
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=10)
+
+print "HUB NAME:" + hubName
+print "DEVICE NAME:" + deviceName
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code: " + str(rc))
+    client.subscribe("devices/" + deviceName + "/messages/devicebound/#")
+
+def on_disconnect(client, userdata, rc):
+    print ("Disconnected with result code: " + str(rc))
+
+def on_message(client, userdata, msg):
+    print (msg.topic+" "+str(msg.payload))
+    # Do this only if you want to send a reply message every time you receive one
+    client.publish("devices/" + deviceName + "/messages/events/", "{id=1}", qos=1)
+
+def on_publish(client, userdata, mid):
+    print ("Sent message")
+
+def readSerial():
+        out = ""
+        while ser.inWaiting() > 0:
+                 out += ser.read(1)
+        return out
+
+def jsonize(installation):
+        # installation is an string containing the values received by the serial link $hum:36$temp:26$lleno:0$fire:0 $type:paper$pos:40.446;79.982
+
+        status = {}
+        default = {"Identificator": "NULL", "Type":"NULL", "Temperature" :"NULL", "Distance":"NULL", "Humidity" : "NULL", "Fire" : "NULL", "Location":"NULL"}
+        fields = installation.split('$')
+        print ("json nodo: " + installation)
+
+        # $id:02$hum:40$temp:26$lleno:0$fire:0
+        # $id:02$type:paper$pos:40.446;79.982
+
+        for element in fields:
+                print "current element: " + element
+                current = element.split(':')
+                if current[0] == "id":
+                        default["Identificator"] = current[1]
+                elif current[0] == "hum":
+                        default["Humidity"] = current[1]
+                elif current[0] == "temp":
+                        default["Temperature"] = current[1]
+                elif current[0] == "lleno":
+                        default["Distance"] = current[1]
+                elif current[0] == "fire":
+                        default["Fire"] = current[1]
+                elif current[0] == "type":
+                        default["Type"] = current[1]
+                elif current[0] == "pos":
+                        default["Location"] = current[1]
+
+        print repr(default)
+
+        return repr(default)
+
+def on_log(client, userdata, level, buf):
+        print("log: ",buf)
+
+
+def loop_forever():
+        while True:
+                time.sleep(1)
+                message = readSerial()
+                print ">>" + message
+                if message != "":
+                        print message
+                        message = jsonize(message)
+                        print message
+                        client.publish("messages/events", message)
+                        #client.send_event_async(message, send_confirmation_callback, 1)
+
+client = mqtt.Client(client_id=deviceName, protocol=mqtt.MQTTv311)
+client.on_log = on_log
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.on_publish = on_publish
+client.loop_forever = loop_forever
+client.username_pw_set(username=hubName + "/" + deviceName, password=SharedAccessSignature)
+#client.tls_insecure_set(False)
+client.tls_set(ca_certs=BaltimoreCyberTrustRootCER, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+client.tls_insecure_set(False)
+client.connect(hubName, port=8883)
+client.loop_forever()
